19f941a7cd6cdc43f62b78bd1f364edafd7f4f3c
Rowdey454 <83260417+Rowdey454@users.noreply.github.com>
2021-09-29 12:58:42 -0500

    Iframe Embed Scripts
    Iframe Embed Scripts

---
+++ AllowedDomainsTCG.ps1
@@ -0,0 +1,39 @@
+﻿Connect-SPOService -Url "Your -admin Sharepoint Online URL here”
+$urls= get-sposite 
+
+
+
+foreach($url in $urls){
+ $SiteURL= $Url.url
+
+#Connect to PnP Online
+Connect-PnPOnline  -Url $SiteURL -UseWebLogin
+$Site = Get-PnPSite -Includes CustomScriptSafeDomains
+
+#This would allow for a custom domain list through powershell
+Write-Host "Applying  AllowedDomains to $SiteURL"
+$site.AllowExternalEmbeddingWrapper = [Microsoft.SharePoint.Client.ScriptSafeExternalEmbedding]::AllowedDomains;
+
+Invoke-pnpquery
+
+
+
+
+ #Add domain
+$Domain = [Microsoft.SharePoint.Client.ScriptSafeDomainEntityData]::new()
+
+$Domain.DomainName = "URL1 here" 
+$Site.CustomScriptSafeDomains.Create($Domain) 
+
+$Domain.DomainName = "URL2 here" 
+$Site.CustomScriptSafeDomains.Create($Domain)
+Invoke-pnpquery
+
+
+
+#Removal of domains
+$Removed=$Site.CustomScriptSafeDomains.GetByDomainName(“URL to be removed here”)
+$Removed.DeleteObject()
+Invoke-pnpquery
+#What URLs need to be deleted?
+}
---
+++ BlockAllTCG.ps1
@@ -0,0 +1,19 @@
+﻿
+Connect-SPOService -Url "Your -Admin Sharepoint Online URL Here" 
+$urls= get-sposite 
+
+
+
+foreach($url in $urls){
+ $SiteURL= $Url.url
+
+
+Connect-PnPOnline -Url $SiteURL  -UseWebLogin
+$Site = Get-PnPSite -Includes CustomScriptSafeDomains
+
+Write-Host "Applying  BlockAll to $SiteURL"
+
+#This blocks any domain  
+$Site.AllowExternalEmbeddingWrapper = [Microsoft.SharePoint.Client.ScriptSafeExternalEmbedding]::None;
+Invoke-PnPQuery
+}
